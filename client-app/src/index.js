84b8d97edbaf3f0b07665004256865b62986bd60
Mohit-Mourya45 <0808cl241117.ies@ipsacademy.org>
2026-06-27 12:26:08 +0530

    first commit

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -8,7 +8,7 @@ import AdminLogin from './AdminViews/AdminLogin';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AdminLogin/>
+    
   <App/>
   </React.StrictMode>
 );
